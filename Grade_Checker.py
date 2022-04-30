3e34ede088e6e6c0f84c5dfc6462a66e6cc970d1
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-04-30 13:03:41 +0300

    Started working on the full table reading

--- Grade_Checker.py
+++ Grade_Checker.py
@@ -35,12 +35,28 @@ agree_button = driver.find_element(By.NAME, AGREE) # Finding the agree element i
 agree_button.send_keys(Keys.RETURN) # Pressing enter on the element
 time.sleep(5) # Waiting 5 seconds for the page to load. And no to be suspicious.
 
-try:
-    output = driver.find_element(By.XPATH, CELL_PATH) # Trying to find the input element in the table.
-    playsound(Credentials.PATH_TO_SOUND) # If the input element is there, there is a grade update and a sound will play.
-    playsound(Credentials.PATH_TO_SOUND)
-except:
-    driver.quit() # If there isn't an input element, there will be an error and the software will quit.
+TABLE_PATH = "/html/body/form[1]/table/tbody"
+#/html/body/form[1]/table/tbody/tr[2]/td[4]
+#/html/body/form[1]/table/tbody/tr[4]/td[4]
+## if /html/body/form[1]/table/tbody/tr[2]/ has "מועד א" check /html/body/form[1]/table/tbody/tr[2]/td[4] 
+# if isn't "אין" add to json file (if not exist - check name of course). if json file is bigger ring the bell. 
+table = driver.find_elements(By.XPATH, TABLE_PATH)
+rows = table[0].find_elements(By.TAG_NAME, "tr")
+for row in rows:
+    col = row.find_elements(By.TAG_NAME, "td")
+    print (col.text)
+time.sleep(4)
+driver.quit()
+# for i in (range(len(table))):
+#     path = ("/html/body/form[1]/table/tbody/tr[2]/td[%d]" %(i+1))
+#     print(driver.find_element(By.XPATH, path).text)
+
+# try:
+#     output = driver.find_element(By.XPATH, CELL_PATH) # Trying to find the input element in the table.
+#     playsound(Credentials.PATH_TO_SOUND) # If the input element is there, there is a grade update and a sound will play.
+#     playsound(Credentials.PATH_TO_SOUND)
+# except:
+#     driver.quit() # If there isn't an input element, there will be an error and the software will quit.
 
 
 
